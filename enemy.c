759f2e2214e270741c56869be89c6206db9a1a8a
Rem_Lawrence <rem00lawrence@gmail.com>
2021-11-10 19:03:05 -0400

    Re-added enemy bullets

--- enemy.c
+++ enemy.c
@@ -79,7 +79,7 @@ void *runEnemy(void *data) {
             srand(time(NULL));   // Initialization, should only be called once.
             if(rand()%8 == 0) {
                 // Returns a pseudo-random integer between 0 and RAND_MAX.
-                //spawnEnemyBullet(e->row+1, e->col, e->p, e->mutex);
+                spawnEnemyBullet(e->row+1, e->col, e->p, e->mutex);
             }
 
             // BulletNode *bulletList = getBulletQueue();
@@ -139,7 +139,7 @@ void *runEnemy(void *data) {
             srand(time(NULL));   // Initialization, should only be called once.
             if(rand()%8 == 0) {
                 // Returns a pseudo-random integer between 0 and RAND_MAX.
-                //spawnEnemyBullet(e->row+2, e->col, e->p, e->mutex);
+                spawnEnemyBullet(e->row+2, e->col, e->p, e->mutex);
             }  
             
             if(e->col <= 0) {
